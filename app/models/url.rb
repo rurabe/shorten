71381ca373a4e8b00657009e2a1f43728bba1792
rurabe <rurabe@gmail.com>
2012-07-20 11:01:11 -0700

    changed base url to heroku

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,7 @@ class Url < ActiveRecord::Base
   attr_accessible :key, :long_url
   
   BRICKS = [(0..9).to_a,("a".."z").to_a,("A".."Z").to_a].flatten!
-  BASE = "http://localhost:3000"#"http://srl.herokuapp.com"
+  BASE = "http://srl.herokuapp.com"
 
    def self.generate_key
      key = []
