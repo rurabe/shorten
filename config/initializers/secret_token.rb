389163798a01d2d41833514f7774eaa9201d8886
rurabe <rurabe@gmail.com>
2012-07-19 20:21:56 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorten::Application.config.secret_token = 'd080ca7cc7ca74524e647e8adcba77944d82bb20ca40e32a7931e82fab5ca03ee78f609f6d4520e846b15d0c5b0ea64abaaebbc2b6f8e615812759782689fb10'
