3e5c137c70897f252f509e336a992dfb8a3c3022
rurabe <rurabe@gmail.com>
2012-07-21 01:28:19 -0700

    built flash messaging system

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -17,8 +17,12 @@ class UrlsController < ApplicationController
   def create
     @url = Url.new(:long_url => params[:long_url], :key => Url.generate_key)
     #todo - error handling
-    @url.save
-    redirect_to url_path(@url)
+    if @url.save
+      redirect_to url_path(@url)
+    else
+      flash[:error] = "Hmmm... I don't recognize that address. :("
+      redirect_to root_path
+    end
   end
   
   def new
