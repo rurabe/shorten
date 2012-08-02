bae2c67cbbf5648335b0d61473852b18575aec05
rurabe <rurabe@gmail.com>
2012-08-02 03:16:41 -0700

    ah the colon

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -2,8 +2,8 @@ module UrlsHelper
 	def full_url(url)
 		full_url = []
 		full_url << request.protocol
-		full_url << request.host + ":"
-		full_url << request.port.to_s unless request.port == 80
+		full_url << request.host 
+		full_url << ":" + request.port.to_s unless request.port == 80
 		full_url <<  "/" 
 		full_url <<  url.key
 		full_url.join("")
