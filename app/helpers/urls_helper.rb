79238dfee4dbadb021420972abbdf6cc40971638
rurabe <rurabe@gmail.com>
2012-08-02 03:13:59 -0700

    removed port from url if == 80

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,5 +1,11 @@
 module UrlsHelper
 	def full_url(url)
-		request.protocol + request.host + ":" + request.port.to_s + "/" + url.key
+		full_url = []
+		full_url << request.protocol
+		full_url << request.host + ":"
+		full_url << request.port.to_s unless request.port == 80
+		full_url <<  "/" 
+		full_url <<  url.key
+		full_url.join("")
 	end
 end
